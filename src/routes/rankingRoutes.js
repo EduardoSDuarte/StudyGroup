5649c907b7de9c4ce45bbcb039f0204ff9c6e070
eduardoo3 <eduardosduarte31@gmail.com>
2026-05-17 18:04:37 -0300

    fix: remove pasta src duplicada da raiz

--- src/routes/rankingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middlewares/authMiddlewares");
-const { ranking, history } = require("../controllers/rankingController");
-
-router.get("/:groupId", verifyToken, ranking);
-router.get("/history/:groupId", verifyToken, history);
-
-module.exports = router;
--- src/routes/timerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middlewares/authMiddlewares");
-const { start, stop } = require("../controllers/timerController");
-
-router.post("/start", verifyToken, start);
-router.post("/stop", verifyToken, stop);
-
-module.exports = router;
