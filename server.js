94a2d0189a21f56bbdc632d4d633617cc3f8ea93
eduardoo3 <eduardosduarte31@gmail.com>
2026-05-17 18:15:27 -0300

    fix: remove arquivos duplicados da raiz

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-require("./src/services/cronService");
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
